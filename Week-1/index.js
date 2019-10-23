e7d62503f564f3e76658a78200452dfba0522f29
atranarta <radyuk.tatyana@gmail.com>
2019-10-23 13:59:56 +0300

    Week 1 howework

---
+++ Week-1/index.js
@@ -0,0 +1,89 @@
+User = class {
+  constructor(name, email) {
+    this.name = name;
+    this.email = email;
+    this.tickets = [];
+  }
+
+  buy(ticket) {
+    this.tickets.push(ticket);
+  }
+
+  printAllTickets() {
+    console.log('Tickets bought by ' + this.name)
+    this.tickets.forEach((ticket) => console.log('for ' + ticket.person.name + ': ' + ticket.eventTitle + ' EUR ' + ticket.price));
+  }
+
+  printTicketsForPerson(person) {
+    console.log('Tickets bought by ' + this.name + ' for ' + person.name + ':');
+    this.tickets
+      .filter((ticket) => ticket.person == person)
+      .forEach((ticket) => console.log(ticket.eventTitle + ' EUR ' + ticket.price));
+  }
+}
+
+Person = class {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+Ticket = class {
+  constructor(person, eventTitle, price) {
+    this.person = person;
+    this.eventTitle = eventTitle;
+    this.price = price;
+  }
+}
+
+
+Event = class {
+  constructor(title, priceAdult, priceReduced) {
+    this.title = title;
+    this.priceAdult = priceAdult;
+    this.priceReduced = priceReduced;
+  }
+
+  createTicket(person) {
+    let price = person.age < 12 ? this.priceReduced : this.priceAdult;
+
+    return new Ticket(person, this.title, price);
+  }
+}
+
+technikmuseum = new Event('Technikmuseum Berlin', '8,00', '4,00');
+zoo = new Event('Berlin Zoological Garden', '15,50', '10,50');
+tierpark = new Event('Tierpark Berlin', '14,50', '9,50');
+naturkunde = new Event('Museum für Naturkunde Berlin', '8,00', '5,00');
+pergamonmuseum = new Event('Pergamonmuseum Berlin', '19', '9,50');
+
+
+user1 = new User('Tanya', 'my@mail.com');
+user2 = new User('Zoidberg', 'Zoidberg@mail.com');
+
+tanya = new Person('Tanya', 30);
+maria = new Person('Maria', 31);
+katya = new Person('Katya', 22);
+katyaJ = new Person('Katya J', 8);
+
+
+user1.buy(zoo.createTicket(tanya));
+user1.buy(zoo.createTicket(maria));
+user1.buy(zoo.createTicket(katya));
+
+user1.buy(tierpark.createTicket(maria));
+
+
+user2.buy(naturkunde.createTicket(tanya));
+user2.buy(pergamonmuseum.createTicket(maria));
+user2.buy(tierpark.createTicket(katya));
+user2.buy(tierpark.createTicket(katyaJ));
+
+
+user1.printAllTickets();
+console.log('=============');
+user1.printTicketsForPerson(maria);
+console.log('=============');
+user2.printAllTickets();
+console.log('=============');
