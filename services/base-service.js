8b3cafa170356fd16ab7a0286f7176fa6bad9150
atranarta <radyuk.tatyana@gmail.com>
2019-11-20 17:34:23 +0100

    fixes

--- services/base-service.js
+++ services/base-service.js
@@ -1,17 +1,22 @@
 module.exports = class Service {
+  constructor(model, dbPath) {
+    this.model = model;
+    this.dbPath = dbPath;
+  }
+
   async findAll() {
-    return this.model.find()
+    return this.model.find();
   }
 
   async add(item) {
-    return this.model.create(item)
+    return this.model.create(item);
   }
 
-  async  del(itemId) {
-    return this.model.remove({ _id: itemId })
+  async del(itemId) {
+    return this.model.remove({ _id: itemId });
   }
 
   async find(itemId) {
-    return this.model.findById(itemId)
+    return this.model.findById(itemId);
   }
-}
+};
