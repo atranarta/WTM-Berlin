92842520bdec3e834f0cd8d4ac78bd76de372e81
atranarta <radyuk.tatyana@gmail.com>
2019-11-20 17:29:25 +0100

    change base-service by using mongoose

--- models/user.js
+++ models/user.js
@@ -1,11 +1,23 @@
-module.exports = class User {
-  constructor(name, age, id) {
-    this.name = name;
-    this.age = age;
-    this.id = id;
-  }
+const mongoose = require('mongoose');
 
-  static create({ name, age, id }) {
-    return new User(name, age, id);
-  }  
-};
+const UserSchema = new mongoose.Schema({
+  name: String,
+  age: Number
+});
+
+const UserModel = mongoose.model('User', UserSchema);
+
+module.exports = UserModel;
+
+
+// module.exports = class User {
+//   constructor(name, age, id) {
+//     this.name = name;
+//     this.age = age;
+//     this.id = id;
+//   }
+
+//   static create({ name, age, id }) {
+//     return new User(name, age, id);
+//   }  
+// };
